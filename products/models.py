4f04b90292bbb5b744b9d319d319f4369027dca3
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-08 20:07:00 +0900

    Modify: 코드 컨벤션 수정

--- products/models.py
+++ products/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db                 import models
 from django.db.models.deletion import CASCADE
 
 class Category(models.Model):
@@ -9,43 +9,43 @@ class Category(models.Model):
 
 class SubCategory(models.Model):
     sub_category = models.CharField(max_length=50, unique=True)
-    category = models.ForeignKey('Category', on_delete=CASCADE)
+    category     = models.ForeignKey('Category', on_delete=CASCADE)
 
     class Meta:
         db_table = 'sub_categories'
 
 class Product(models.Model):
-    korean_name = models.CharField(max_length=50, unique=True)
+    korean_name  = models.CharField(max_length=50, unique=True)
     english_name = models.CharField(max_length=50, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=100, unique=True)
+    price        = models.DecimalField(max_digits=10, decimal_places=2)
+    description  = models.CharField(max_length=100, unique=True)
     sub_category = models.ForeignKey('SubCategory', on_delete=CASCADE)
 
     class Meta:
         db_table = 'products'
 
 class ProductInfo(models.Model):
-    meterial = models.CharField(max_length=50)
-    size = models.CharField(max_length=50)
-    manufaturer = models.CharField(max_length=50)
-    made = models.CharField(max_length=50)
-    distributor = models.CharField(max_length=50)
+    meterial     = models.CharField(max_length=50)
+    size         = models.CharField(max_length=50)
+    manufaturer  = models.CharField(max_length=50)
+    made         = models.CharField(max_length=50)
+    distributor  = models.CharField(max_length=50)
     afterservice = models.CharField(max_length=50)
-    product = models.OneToOneField('Product', on_delete=CASCADE)
+    product      = models.OneToOneField('Product', on_delete=CASCADE)
 
     class Meta:
         db_table = 'product_infos'
 
 class ImageType(models.Model):
     image_type = models.IntegerField()
-    product = models.ManyToManyField('Product', through='ProductImage')
+    product    = models.ManyToManyField('Product', through='ProductImage')
 
     class Meta:
         db_table = 'image_types'
 
 class ProductImage(models.Model):
-    image_url = models.CharField(max_length=200)
-    product = models.ForeignKey('Product', on_delete=CASCADE)
+    image_url  = models.CharField(max_length=200)
+    product    = models.ForeignKey('Product', on_delete=CASCADE)
     image_type = models.ForeignKey('ImageType', on_delete=CASCADE)
 
     class Meta:
--- users/models.py
+++ users/models.py
@@ -1,22 +1,21 @@
-from django.db import models
+from django.db                 import models
 from django.db.models.deletion import CASCADE
 
 class User(models.Model):
-    sex           = models.IntegerField()
-    name          = models.CharField(max_length=50)
-    password      = models.CharField(max_length=100)
-    email         = models.CharField(max_length=50, unique=True)
-    mobile        = models.CharField(max_length=50, unique=True)
-    profile_image = models.CharField(max_length=200, blank=True)
-    product_like  = models.ManyToManyField('products.Product', through='Like', related_name='product_like')
+    sex                = models.IntegerField()
+    name               = models.CharField(max_length=50)
+    password           = models.CharField(max_length=100)
+    email              = models.CharField(max_length=50, unique=True)
+    mobile             = models.CharField(max_length=50, unique=True)
+    profile_image      = models.CharField(max_length=200, blank=True)
+    product_like       = models.ManyToManyField('products.Product', through='Like', related_name='product_like')
     product_order_item = models.ManyToManyField('products.Product', through='orders.OrderItem', related_name='product_order_item')
 
     class Meta:
         db_table = 'users'                                                                                                                                                              
 
-
 class Like(models.Model):
-    users = models.ForeignKey('User', on_delete=CASCADE)
+    users    = models.ForeignKey('User', on_delete=CASCADE)
     products = models.ForeignKey('products.Product', on_delete=CASCADE)
 
     class Meta:
