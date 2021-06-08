aa69cc3e9eba3d3f99a30936520ca36bc0fcf3bd
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-08 15:03:07 +0900

    Add: users-model 작성

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    sex           = models.IntegerField(null=True)
+    profile_image = models.CharField(max_length=200)
+    password      = models.CharField(max_length=100, null=True)
+    email         = models.CharField(max_length=50, unique=True, null=True)
+    mobile        = models.CharField(max_length=50, unique=True, null=True)
+    name          = models.CharField(max_length=50, unique=True, null=True)
+
+    class Meta:
+        db_table = 'users'
