90e9cc21a6f36acafa22d2eb165ca8d770a9a6b1
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-09 09:24:11 +0900

    Add: 로그인 뷰 작성 1. 패스워드 bcrypt 적용하여 암호화 2. 토큰 발행 기능 추가

--- users/views.py
+++ users/views.py
@@ -1,19 +1,22 @@
-import json, re
+import json, re, bcrypt, jwt
 from django.http.response import JsonResponse
 
 from django.views import View
 from django.http  import JsonResponse
 from django.db    import IntegrityError
 
-from .models      import User
+from .models                import User
+from seolleungbeer.settings import SECRET_KEY, ALGORITHM
 
 class SignupView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
+            
+            bcrypt_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             password_regx = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{10,}$")
-            email_regex  = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            email_regex   = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
             if User.objects.filter(email = data['email']).exists():
                 return JsonResponse({'message': 'EMAIL_EXIST'}, status=400)
@@ -32,7 +35,7 @@ class SignupView(View):
                 name     = data['name'],
                 email    = data['email'],
                 mobile   = data['mobile'],
-                password = data['password']
+                password = bcrypt_password
             )
 
             return JsonResponse({'message': 'SUCCESS'}, status=201)
@@ -42,3 +45,27 @@ class SignupView(View):
         
         except IntegrityError:
             return JsonResponse({'message': 'INTEGRITY_ERROR'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            input_email    = data['email']
+            input_password = data['password'].encode('utf-8')
+
+            if input_email == '' or input_password == '':
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            
+            db_email    = User.objects.get(email=input_email)
+            db_password = db_email.password.encode('utf-8')
+
+            if not bcrypt.checkpw(input_password, db_password):
+                return JsonResponse({'message': 'INVALID_PASSWORD'}, status=401)
+            
+            token = jwt.encode({'user_id': db_email.id}, SECRET_KEY, ALGORITHM)
+            
+            return JsonResponse({'token': token, 'message': 'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
