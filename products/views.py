90c85a650226f250f1244d98cfa81173d6547b80
kimkyoungcheon <rudcjs32@gmail.com>
2021-06-10 15:49:27 +0900

    Add: 상품상세페이지로직 완성 - 상품상세페이지 뷰 완성 - Path parameter로 상품id를 이용하여 데이터전달 - 프론트님과 상의하여 result 리스트에서 info 딕셔너리를 생성하여 데이터나눠서전달

--- products/views.py
+++ products/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from .models import Product, ProductImage, ProductInfo, ImageType,SubCategory
+
+class ProductDetail(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+            result=[]
+            images = product.productimage_set.all()
+            image_list = [{
+                'image_url'  : image.image_url,
+                'image_type' : image.image_type.type
+            }for image in images]
+
+            result.append({
+                'korean_name'  : product.korean_name,
+                'english_name' : product.english_name,
+                'price'        : product.price,
+                'description'  : product.description,
+                'image'        : image_list,
+
+                'info'         :{
+                    'meterial'     : product.product_info.meterial,
+                    'size'         : product.product_info.size,
+                    'manufacturer' : product.product_info.manufacturer,
+                    'made'         : product.product_info.made,
+                    'distributor'  : product.product_info.distributor,
+                    'afterservice' : product.product_info.afterservice
+                }})
+
+            return JsonResponse({'result':result},status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'},status=400)
