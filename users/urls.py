f54507ddd37ddf6c72791afcfcbc2b8bfe695f3f
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-11 15:43:43 +0900

    Refactor: 브랜치 분리 1. 로그인 기능 2. 중복검사 기능 3. 데코레이터 기능 4. 위 기능별 브랜치 분리

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views      import SignupView, LoginView, EmailCheckView
+from .views      import SignupView, LoginView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
-    path('/login', LoginView.as_view()),
-    path('/email-check', EmailCheckView.as_view())
+    path('/login', LoginView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -6,6 +6,8 @@ from django.http            import JsonResponse
 from .models                import User
 from seolleungbeer.settings import SECRET_KEY, ALGORITHM
 
+password_regx = re.compile('^.*(?=^.{10,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$')
+
 class SignupView(View):
     def post(self, request):
         try:
@@ -13,22 +15,9 @@ class SignupView(View):
 
             bcrypt_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-            password_regx = re.compile('^.*(?=^.{10,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$')
-            email_regex   = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-            mobile_regex  = re.compile('^[0-9]{10,11}$')
-
-            if User.objects.filter(mobile = data['mobile']).exists():
-                return JsonResponse({'message': 'MOBILE_EXIST'}, status=400)
-
-            if not email_regex.match(data['email']):
-                return JsonResponse({'message':'PLEASE ENTER @ OR .'}, status=400)
-            
             if not password_regx.match(data['password']):
                 return JsonResponse({'message':'INVALID_PASSWORD'}, status=400)
-
-            if not mobile_regex.match(data['mobile']):
-                return JsonResponse({'message': 'INVALID_MOBILE'}, status=400)
-
+            
             User.objects.create(
                 sex      = data['sex'],
                 name     = data['name'],
@@ -50,31 +39,18 @@ class LoginView(View):
             input_email    = data['email']
             input_password = data['password'].encode('utf-8')
 
-            if input_email == '' or input_password == '':
-                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            if not User.objects.filter(email = data['email']).exists():
+                return JsonResponse({'message': 'INVALID_USER'}, status=400)
             
-            db_email    = User.objects.get(email=input_email)
+            db_email       = User.objects.get(email=input_email)
             db_password = db_email.password.encode('utf-8')
 
             if not bcrypt.checkpw(input_password, db_password):
-                return JsonResponse({'message': 'INVALID_PASSWORD'}, status=401)
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
             
-            token = jwt.encode({'user_id': db_email.id}, SECRET_KEY, ALGORITHM)
+            access_token = jwt.encode({'user_id': db_email.id}, SECRET_KEY, ALGORITHM)
             
-            return JsonResponse({'token': token, 'message': 'SUCCESS'}, status=200)
-
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
-
-class EmailCheckView(View):
-    def post(self, request):
-        try:
-            data = json.loads(request.body)
-
-            if User.objects.filter(email = data['email']).exists():
-                return JsonResponse({'message': 'EMAIL_EXIST'}, status=400)
-
-            return JsonResponse({'message': 'SUCCESS'}, status=200)
+            return JsonResponse({'token': access_token, 'message': 'SUCCESS'}, status=200)
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
