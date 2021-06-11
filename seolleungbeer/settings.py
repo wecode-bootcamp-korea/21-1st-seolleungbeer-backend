cc4c297b4cb382edbdab54d85980ab26f75ddc75
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-11 16:28:26 +0900

    Add: 이메일, 전화번호 중복 검사 기능 추가

--- seolleungbeer/settings.py
+++ seolleungbeer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from my_settings import  SECRET_KEY, DATABASES
+from my_settings import  SECRET_KEY, DATABASES, ALGORITHM
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY
+
+ALGORITHM = ALGORITHM
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- users/views.py
+++ users/views.py
@@ -1,40 +1,90 @@
-import json, re
+import json, re, bcrypt, jwt
 
-from django.views import View
-from django.http  import JsonResponse
-from django.db    import IntegrityError
+from django.views           import View
+from django.http            import JsonResponse
 
-from .models      import User
+from .models                import User
+from seolleungbeer.settings import SECRET_KEY, ALGORITHM
+
+password_regx = re.compile('^.*(?=^.{10,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$')
+email_regex   = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+mobile_regex  = re.compile('^[0-9]{10,11}$')
+
+class EmailCheckView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if User.objects.filter(email = data['email']).exists():
+                return JsonResponse({'message': 'EMAIL_EXIST'}, status=400)
+            
+            if not email_regex.match(data['email']):
+                return JsonResponse({'message':'INVALID_EMAIL'}, status=400)
+
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+class MobileCheckView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+           
+            if User.objects.filter(mobile = data['mobile']).exists():
+                return JsonResponse({'message': 'MOBILE_EXIST'}, status=400)
+            
+            if not mobile_regex.match(data['mobile']):
+                return JsonResponse({'message': 'INVALID_MOBILE'}, status=400)
+
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
 class SignupView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
 
-            password_regx = re.compile('^.*(?=^.{10,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$')
-            email_regex   = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-
-            if User.objects.filter(email = data['email']).exists():
-                return JsonResponse({'message': 'EMAIL_EXIST'}, status=400)
-
-            if User.objects.filter(mobile = data['mobile']).exists():
-                return JsonResponse({'message': 'MOBILE_EXIST'}, status=400)
-
-            if not email_regex.match(data['email']):
-                return JsonResponse({'message':'INVALID_EMAIL'}, status=400)
+            bcrypt_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             if not password_regx.match(data['password']):
                 return JsonResponse({'message':'INVALID_PASSWORD'}, status=400)
-
+            
             User.objects.create(
                 sex      = data['sex'],
                 name     = data['name'],
                 email    = data['email'],
                 mobile   = data['mobile'],
-                password = data['password']
+                password = bcrypt_password
             )
 
             return JsonResponse({'message': 'SUCCESS'}, status=201)
+        
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            input_email    = data['email']
+            input_password = data['password'].encode('utf-8')
+
+            if not User.objects.filter(email = data['email']).exists():
+                return JsonResponse({'message': 'INVALID_USER'}, status=400)
+            
+            db_email       = User.objects.get(email=input_email)
+            db_password = db_email.password.encode('utf-8')
+
+            if not bcrypt.checkpw(input_password, db_password):
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            
+            access_token = jwt.encode({'user_id': db_email.id}, SECRET_KEY, ALGORITHM)
+            
+            return JsonResponse({'token': access_token, 'message': 'SUCCESS'}, status=200)
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
