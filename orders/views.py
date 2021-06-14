9e8c0c0353a2ffd14743e5396f08474a098b6cdd
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-14 21:05:49 +0900

    Modify: order.views 코드 수정

--- orders/views.py
+++ orders/views.py
@@ -2,31 +2,29 @@ from django.views    import View
 from django.http     import JsonResponse
 
 from .models         import Order, OrderItem
-from products.models import ProductImage
-
-from users.utils import user_decorator
+from users.utils     import user_decorator
 
 class CartView(View):
     @user_decorator
     def get(self, request):
         try:
             user       = request.user
-            order_item = OrderItem.objects.filter(orders__user = user, order__order_status__status = '주문 전')
+            order_item = OrderItem.objects.filter(order__user = user, order__order_status__status="주문 전")
+            result     = []
 
             for carts in order_item:
                 carts_list = {
                     'cart_id'         : carts.id,
-                    'product_image'   : ProductImage.image_url,
                     'amount'          : carts.amount,
                     'korean_name'     : carts.product.korean_name,
                     'english_name'    : carts.product.english_name,
                     'delivery_charge' : carts.order.delivery_charge,
-                    'payment_charge'  : carts.order.payment_charge,
-                    'delivery_method' : carts.order.delivery_method,
-                    'like'            : carts.order.user.like
+                    'payment_charge'  : carts.product.price,
+                    'delivery_method' : carts.order.delivery_method
                 }
+                result.append(carts_list)
 
-            return JsonResponse({'message':'SUCCESS', 'result':carts_list}, status=200)
+            return JsonResponse({'message':'SUCCESS', 'result':result}, status=200)
         
         except Order.DoesNotExist:
             return JsonResponse({'message': 'NOTHING_IN_CART'}, status=400)
