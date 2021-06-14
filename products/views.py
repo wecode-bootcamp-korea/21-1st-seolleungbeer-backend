a281d106ea7ec2fd65b1695c0523f8d8b63c3adb
anne <annewithanme@gmail.com>
2021-06-15 02:59:14 +0900

    Modify: 리뷰 반영

--- products/views.py
+++ products/views.py
@@ -1,97 +1,59 @@
 from itertools             import chain
+from django.db.models.expressions import OuterRef
+from django.db.models.fields import CharField
+from django.db.models.query_utils import subclasses
 
 from django.views          import View
 from django.http           import JsonResponse
 from django.core.paginator import EmptyPage
+from django.db.models      import Q, When, Case, Exists,Value
 
 from .models import Category, SubCategory, Product, ProductInfo, ProductImage, ImageType
 
 class CategoryListView(View):
     def get(self, request):
-        try:
-            categories = Category.objects.all()
-            result     = {}
+        categories = Category.objects.all()
+        result     = {}
 
-            for category in categories:
-                subcategories = SubCategory.objects.filter(category=category)
-                result[category.english_name] = [sub.english_name for sub in subcategories]
+        for category in categories:
+            subcategories = SubCategory.objects.filter(category=category)
+            result[category.english_name] = [sub.english_name for sub in subcategories]
 
-            return JsonResponse(result, status=200)
-        
-        except Category.DoesNotExist or SubCategory.DoesNotExist:
-            return JsonResponse({'message':'Category Does Not Exist'}, status=400)
+        return JsonResponse(result, status=200)
 
 class ProductListView(View):
     def get(self, request):
-        try:        
-            # get_query_parameter
-            offset        = int(request.GET.get('offset') or 0)
-            limit         = int(request.GET.get('limit') or 6)
-            category      = request.GET.get('category')
-            subcategories = request.GET.get('subcategory')
-    
-            # filter
-            products = []
+        offset        = int(request.GET.get('offset',0))
+        limit         = int(request.GET.get('limit',6))
+        category      = request.GET.get('category')
+        subcategories = request.GET.get('subcategory')
 
-            if category == '' or category == None:
-                products = Product.objects.all()
-            elif subcategories == '' or subcategories == None:
-                category       = Category.objects.get(english_name=category)
-                subcategories  = SubCategory.objects.filter(category=category)
-                products       = []
+        if subcategories:
+            q = Q(sub_category__english_name=subcategories)
+        elif category:
+            q = Q(sub_category__category__english_name=category)
+        else:
+            q = Q()
 
-                for sub in subcategories:
-                    products_in_subs = Product.objects.filter(sub_category=sub)                   
-                    products = list(chain(products, products_in_subs))
-            else:
-                subcategories = SubCategory.objects.get(english_name=subcategories)
-                products      = Product.objects.filter(sub_category=subcategories)
+        products = Product.objects.filter(q).order_by('id')
+        products_in_page = products[offset:(offset+limit)]
+        message = int(products.count() <= (offset+limit))
+        result = {}
+        result_list = []
 
-            # check_product_amount        
-            if len(products) == 0:
-                return JsonResponse({'message':'Product Does Not Exist'}, status=400)      
-            elif len(products) <= offset:
-                return JsonResponse({'message':'EmptyPage'}, status=404)            
-            elif len(products) <= offset+limit:
-                message = 'Last Page'
-            else:
-                message = 'Page Left'
+        for product in products_in_page:
+            image = product.productimage_set.filter(image_type__type="1000*1000").first()
+            image_url = image.image_url if image else image
+            result = {
+                'id'           : product.id,
+                'korean_name'  : product.korean_name,
+                'english_name' : product.english_name,
+                'price'        : product.price,
+                'image'        : image_url
+            }
+            result_list.append(result)
 
-            # paging
-            products_in_page = products[offset:(offset+limit)]
-
-            # make_response
-            result      = {}
-            result_list = []
-
-            for product in products_in_page:
-                image_type  = ImageType.objects.get(type="1000*1000")
-                image       = ProductImage.objects.filter(product=product, image_type=image_type)
-
-                # chechk_image_amount
-                if len(image) == 0:
-                    image_url = "No Image"                  
-                elif len(image) == 1:
-                    image_url = image[0].image_url
-                else:
-                    return JsonResponse({'message':'Image Multiple Objects Returned'}, status=400)
-
-                result = {
-                    'id'           : product.id,
-                    'korean_name'  : product.korean_name,
-                    'english_name' : product.english_name,
-                    'price'        : product.price,
-                    'image'        : image_url
-                }
-                result_list.append(result)
-            
-            return JsonResponse({'message':message,'content':result_list}, status=200)
-        
-        except Category.DoesNotExist or SubCategory.DoesNotExist:
-            return JsonResponse({'message':'Category Does Not Exist'}, status=400)
-
-        except Category.MultipleObjectsReturned or SubCategory.MultipleObjectsReturned:
-            return JsonResponse({'message':'Category Multiple Objects Returned'}, status=400)
+        return JsonResponse({'message':message, 'result':result_list}, status=200)
 
 class ProductDetail(View):
     def get(self, request, product_id):
