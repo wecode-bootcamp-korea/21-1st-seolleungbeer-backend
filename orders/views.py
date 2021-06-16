010dc1afdabd59c3e01180b21c273d5c0b0faa7e
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-16 09:36:09 +0900

    Add: DB데이터 프론트로 반환 1. 삭제된 상품 제외한 아이템 목록 프론트로 전송

--- orders/views.py
+++ orders/views.py
@@ -9,8 +9,15 @@ from orders.models   import OrderItem
 class CartView(View):
     @user_decorator
     def delete(self, request):
-            data = json.loads(request.body)
+            data       = json.loads(request.body)
+            order_item = OrderItem.objects.filter(id__in=data['cart_id'])
 
-            OrderItem.objects.filter(id__in=data['cart_id']).delete()
+            if data['cart_id'] not in order_item:
+                return JsonResponse({'message': 'NOT_FOUND'}, status=404)
+
+            order_item.delete()
+
+            rest_value = OrderItem.objects.all()
+            rest_value = list(rest_value.values())
             
-            return JsonResponse({'message':'DELETE_SUCCESS'}, status=200)
+            return JsonResponse({'message':'DELETE_SUCCESS', 'rest_value':rest_value}, status=200)
