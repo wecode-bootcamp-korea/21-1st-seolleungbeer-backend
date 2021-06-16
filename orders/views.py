74e14332ab721a2ae1cdbd93f11a96087a12b59c
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-16 15:21:15 +0900

    Refactor: 불필요한 코드 제거

--- orders/views.py
+++ orders/views.py
@@ -37,18 +37,14 @@ class CartView(View):
     def delete(self, request):
         try:
             data       = json.loads(request.body)
-            db_item    = OrderItem.objects.filter(
-                order_id=data['order_id'], 
-                order__order_status_id = OrderStatus.PENDING
-            )
             order_item = OrderItem.objects.filter(id__in=data['cart_item_id'])
 
             order_item.delete()
-
-            rest_value = db_item.all()
-            rest_value = list(rest_value.values())
             
-            return JsonResponse({'message':'DELETE_SUCCESS', 'rest_value':rest_value}, status=200)
+            return JsonResponse({'message':'DELETE_SUCCESS'}, status=200)
         
         except KeyError:
             return JsonResponse({'message':'INVAILD_VALUE'}, status=400)
+        
+        except ValueError:
+            return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
