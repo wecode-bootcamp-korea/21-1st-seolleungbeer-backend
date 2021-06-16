500a10e8b1fa890f9a0c483de7189ab8f101db08
HWANG BOK SIL <tlatms0908@gmail.com>
2021-06-16 10:19:36 +0900

    Refactor: 불필요 코드 제거

--- orders/views.py
+++ orders/views.py
@@ -1,23 +1,28 @@
 import json
 
-from django.views    import View
-from django.http     import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
 
 from users.utils     import user_decorator
-from orders.models   import OrderItem
+from orders.models   import OrderItem, OrderStatus
 
 class CartView(View):
     @user_decorator
     def delete(self, request):
+        try:
             data       = json.loads(request.body)
-            order_item = OrderItem.objects.filter(id__in=data['cart_id'])
-
-            if data['cart_id'] not in order_item:
-                return JsonResponse({'message': 'NOT_FOUND'}, status=404)
+            db_item    = OrderItem.objects.filter(
+                order_id=data['order_id'], 
+                order__order_status_id = OrderStatus.PENDING
+            )
+            order_item = OrderItem.objects.filter(id__in=data['cart_item_id'])
 
             order_item.delete()
 
-            rest_value = OrderItem.objects.all()
+            rest_value = db_item.all()
             rest_value = list(rest_value.values())
             
             return JsonResponse({'message':'DELETE_SUCCESS', 'rest_value':rest_value}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'message':'INVAILD_VALUE'}, status=400)
